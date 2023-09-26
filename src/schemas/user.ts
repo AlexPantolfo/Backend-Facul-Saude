1b5b8aea2c6da8d1e59e43a26f340e08669c55ce
celsocontim <celso.contim@hotmail.com>
2023-09-26 09:32:10 -0300

    Consertos/TODO's de usuarios

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -3,20 +3,32 @@ import mongoose from "mongoose";
 /*import bcrypt from "bcrypt";*/
 
 const UserSchema = new mongoose.Schema({
+	 nome: {
+   	  type: String,
+   	  required: [true, "Nome é obrigatório"],
+    },
     email: {
         type: String,
         unique: true,
-        require: true
+        require: [true, "Email é obrigatório"],
     },
     password: {
         type: String,
-        require: true,
+        require: [true, "Senha obrigatória"],
+    },
+    dataNascimento: {
+        type: String,
+        require: [true, "DataNascimento obrigatória"],
     },
     dependentes: [{
         nome: String,
         email: String,
         dataNascimento: String
     }]
+    /* Se estiver dificil de validar esse array nas responses/dto
+       Usar dependente como usuário normal e adicionar os campos
+       isDependente (boolean, required) e idTitular (ObjectID) ao schema
+    */
 });
 
 UserSchema.pre('save', async function (next) {
